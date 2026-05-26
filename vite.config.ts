4d7593fbd613ee633d6cbe4b6abc3639e1e24396
Amardeep Limbu <limbuamardeep18@gmail.com>
2026-05-26 14:33:24 +0545

    third-commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: 'autoUpdate',
-    manifest: {
-      name: 'zustand store',
-      short_name: 'store',
-      description: 'My zustand app',
-      start_url: '/',
-      scope: '/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#42b883',
-      icons: [
-        { src: '/pwa-192.png', sizes: '192x192', type: 'image/png' },
-        { src: '/pwa-512.png', sizes: '512x512', type: 'image/png' },
-      ],
-    },
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      runtimeCaching: [
-        {
-          urlPattern: /.*/, // all requests — same as your fetch listener
-          handler: 'NetworkFirst', // try network → clone → cache → return res
-          options: {
-            cacheName: 'v1',
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "zustand store",
+        short_name: "store",
+        description: "My zustand app",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#42b883",
+        icons: [
+          { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
+          { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: /.*/, // all requests
+            handler: "NetworkFirst", // try network if network available clone and if no network fetch clone from cache
+            options: {
+              cacheName: "v1",
+            },
           },
-        },
-      ],
-    },
-  })],
+        ],
+      },
+    }),
+  ],
 });
